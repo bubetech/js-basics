774364a09356ef601da1c1e617e0fd06a33d1f06
bubetech <84184648+bubetech@users.noreply.github.com>
2021-05-20 19:14:14 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// funtion that returns dollar as naira value where dollar = 410.83
+// where dollar=d and naira = n
+function dollarVal (d,n){
+    const product = d*n;
+    return product
+}
+console.log(dollarVal(10,410.83))
